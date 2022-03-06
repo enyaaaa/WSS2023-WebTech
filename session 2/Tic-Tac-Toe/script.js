32d76db3b978ac5c989051331589b25caa552596
enyaaaa <enya.tan3009@gmail.com>
2022-03-07 00:02:20 +0800

    session 2

--- session 2/Tic-Tac-Toe/script.js
+++ session 2/Tic-Tac-Toe/script.js
@@ -1,127 +1,88 @@
-// HTML Elements
-const statusDiv = document.querySelector('.playerturn');
-const resetDiv = document.querySelector('.reset');
-const cellDivs = document.querySelectorAll('.box');
+const statusDisplay = document.querySelector('.players');
 
-// game constants
-const xSymbol = 'X';
-const oSymbol = 'O';
+let gameActive = true;
+let currentPlayer = "X";
+let game = ["", "", "", "", "", "", "", "", ""];
 
-// game variables
-let gameIsLive = true;
-let xIsNext = true;
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
+statusDisplay.innerHTML = currentPlayerTurn();
 
-// functions
-const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-const handleWin = (letter) => {
-    gameIsLive = false;
-    if (letter === 'x') {
-        statusDiv.innerHTML = `${letterToSymbol(letter)} has won!`;
-    } else {
-        statusDiv.innerHTML = `<span>${letterToSymbol(letter)} has won!</span>`;
-    }
-};
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    game[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
 
-const checkGameStatus = () => {
-    const topLeft = cellDivs[0].classList[1];
-    const topMiddle = cellDivs[1].classList[1];
-    const topRight = cellDivs[2].classList[1];
-    const middleLeft = cellDivs[3].classList[1];
-    const middleMiddle = cellDivs[4].classList[1];
-    const middleRight = cellDivs[5].classList[1];
-    const bottomLeft = cellDivs[6].classList[1];
-    const bottomMiddle = cellDivs[7].classList[1];
-    const bottomRight = cellDivs[8].classList[1];
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
 
-    // check winner
-    if (topLeft && topLeft === topMiddle && topLeft === topRight) {
-        handleWin(topLeft);
-        cellDivs[0].classList.add('won');
-        cellDivs[1].classList.add('won');
-        cellDivs[2].classList.add('won');
-    } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
-        handleWin(middleLeft);
-        cellDivs[3].classList.add('won');
-        cellDivs[4].classList.add('won');
-        cellDivs[5].classList.add('won');
-    } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
-        handleWin(bottomLeft);
-        cellDivs[6].classList.add('won');
-        cellDivs[7].classList.add('won');
-        cellDivs[8].classList.add('won');
-    } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
-        handleWin(topLeft);
-        cellDivs[0].classList.add('won');
-        cellDivs[3].classList.add('won');
-        cellDivs[6].classList.add('won');
-    } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
-        handleWin(topMiddle);
-        cellDivs[1].classList.add('won');
-        cellDivs[4].classList.add('won');
-        cellDivs[7].classList.add('won');
-    } else if (topRight && topRight === middleRight && topRight === bottomRight) {
-        handleWin(topRight);
-        cellDivs[2].classList.add('won');
-        cellDivs[5].classList.add('won');
-        cellDivs[8].classList.add('won');
-    } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
-        handleWin(topLeft);
-        cellDivs[0].classList.add('won');
-        cellDivs[4].classList.add('won');
-        cellDivs[8].classList.add('won');
-    } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
-        handleWin(topRight);
-        cellDivs[2].classList.add('won');
-        cellDivs[4].classList.add('won');
-        cellDivs[6].classList.add('won');
-    } else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle && middleRight && bottomLeft && bottomMiddle && bottomRight) {
-        gameIsLive = false;
-        statusDiv.innerHTML = 'Game is tied!';
-    } else {
-        xIsNext = !xIsNext;
-        if (xIsNext) {
-            statusDiv.innerHTML = `${xSymbol} is next`;
-        } else {
-            statusDiv.innerHTML = `<span>${oSymbol} is next</span>`;
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = game[winCondition[0]];
+        let b = game[winCondition[1]];
+        let c = game[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
         }
     }
-};
 
-
-// event Handlers
-const handleReset = () => {
-    xIsNext = true;
-    statusDiv.innerHTML = `${xSymbol} is next`;
-    for (const cellDiv of cellDivs) {
-        cellDiv.classList.remove('x');
-        cellDiv.classList.remove('o');
-        cellDiv.classList.remove('won');
-    }
-    gameIsLive = true;
-};
-
-const handleCellClick = (e) => {
-    const classList = e.target.classList;
-
-    if (!gameIsLive || classList[1] === 'x' || classList[1] === 'o') {
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
         return;
     }
 
-    if (xIsNext) {
-        classList.add('x');
-        checkGameStatus();
-    } else {
-        classList.add('o');
-        checkGameStatus();
+    let roundDraw = !game.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
     }
-};
 
-
-// event listeners
-resetDiv.addEventListener('click', handleReset);
-
-for (const cellDiv of cellDivs) {
-    cellDiv.addEventListener('click', handleCellClick)
+    handlePlayerChange();
 }
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (game[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    game = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.box').forEach(cell => cell.innerHTML = "");
+}
+
+
+document.querySelectorAll('.box').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.reset').addEventListener('click', handleRestartGame);
